67edf4c454ce4d92a38b3cc6a059c1d67688a8a0
Alexis <alexisdavalos.tech@gmail.com>
2020-05-07 21:53:23 -0400

    search, get, getall, drop, dropall, help commands

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,4 @@
-from colors import Colors
+from colors import Colors as c
 from room import Room
 from player import Player
 # Declare all the rooms
@@ -8,7 +8,7 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ['rope', 'hammer', 'chisel']),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -63,74 +63,130 @@ def get_key(val):
 # player approaches function
 
 
-def player_approaches(player):
-    print(f'{Colors.OKBLUE}Player approaches {player.location.name}...\n{player.location.description}{Colors.ENDC}')
+def playerAproaches(player):
+    print(f'{c.OKBLUE}Player approaches {player.location.name}...\n{player.location.description}{c.ENDC}\n')
+
+
+def playerLocation(player):
+    print(f'{c.UNDERLINE}{c.WARNING}{c.BOLD}Current Location: {player.location.name}{c.ENDC}')
+
+
+def printError():
+    print(f'\n{c.FAIL}Command not recognized. Please try again{c.ENDC}\n')
+
+
+def printCommands():
+    commands = {
+        'help': 'displays a list of commands',
+        'q': 'command to quit instance',
+        'player': 'displays player information',
+        'search': 'searches the current location for items',
+        'get': 'use syntax: get [item] after finding items to add item to player inventory',
+        'get all': 'adds all items in current location to player inventory',
+        'drop': 'use syntax: drop [item] to drop an item on the ground',
+    }
+    print(f'\n|---------- Adventure Game Commands ----------|\n')
+    for (k, v) in commands.items():
+        print(f' - {c.BOLD}{k}{c.ENDC}: {v}')
+    print(f'\n|-------------------- End --------------------|\n')
 
 
 while True:
     # prompts user to create new player
     if not player:
         prompt = input(
-            f'\n{Colors.WARNING}*** WELCOME TO THE ADVENTURE GAME ***\n \nCreate a new character by typing in a name:{Colors.ENDC} ')
+            f'\n{c.WARNING}*** WELCOME TO THE ADVENTURE GAME ***\n \nCreate a new character by typing in a name: ')
         if prompt == "q":
             print("Goodbye! Thanks for playing!")
             break
         else:
             player = Player(prompt, room['outside'])
             print(
-                f'\n{Colors.OKGREEN}*** Created New Player ***\n\n{player}\n\n{Colors.ENDC}')
-            currentRoom = get_key(player.location)
+                f'\n{c.OKGREEN}*** Created New Player ***\n\n{player}\n\n{c.ENDC}')
+            # currentRoom = get_key(player.location)
     # adventure game starts
     else:
         try:
-            # initial prompt
-            print(
-                f'{Colors.UNDERLINE}{Colors.WARNING}Current Location: {player.location.name}{Colors.ENDC}')
+            # displays current user location
+            playerLocation(player)
+            # initialize input prompt
             prompt = input(
-                f'\nEnter Command or type "help" for a list of commands: ')
-            # exit prompt
-            if prompt.lower() == "q":
-                print("Goodbye! Thanks for playing!")
-                break
+                f'\nEnter Command or type "help" for a list of commands: ').split()
+            # Player Navigation
+            if prompt[0].lower() in ("n", "s", "e", "w"):
+                # check if navigation is possible
+                if hasattr(player.location, f'{prompt[0]}_to'):
+                    # update player location
+                    player.setLocation(
+                        getattr(player.location, f'{prompt[0]}_to'))
+                    # print new location
+                    print(f'\n{c.HEADER}Navigating......\n{c.ENDC}')
+                    playerAproaches(player)
+                else:
+                    # print location not available
+                    print('\nYou see nothing interesting in that direction..')
+            # Player Room Search & Item pickup
+            elif prompt[0].lower() == "search":
+
+                print(f'\nYou begin searching the room....')
+                # checks the room for items
+                if not player.location.items:
+                    print(
+                        f'\n{c.FAIL}You fail to find any items{c.ENDC}\n')
+                else:
+                    print(
+                        f'\n{c.OKGREEN}Aha! You find: {player.location.items}')
+                    print(
+                        f'\n{c.OKBLUE}Type "get [item]" to store item in your inventory OR Type "get all" to store all items in your inventory')
+                    # prompt user to enter command
+                    choice = input(f'\nWhat will you do?: ').split()
+                    # check length of user input
+                    if len(choice) == 1:
+                        # exits loop if q
+                        if choice[0] == "q":
+                            pass
+                        # prints error
+                        else:
+                            printError()
+                    # picks up all items
+                    elif choice[1].lower() == "all":
+                        player.pickUpAll(player.location.items)
+                        player.location.items = []
+                    # picks up single item
+                    elif choice[1].lower() in player.location.items:
+                        # checks if user choice is in room
+                        for item in player.location.items:
+                            # there's a match
+                            if item == choice[1].lower():
+                                # adds to user inventory
+                                player.pickUp(item)
+                                # removes item from room
+                                player.location.items.remove(item)
+                    # prints error
+                    else:
+                        printError()
+            # player drops item
+            elif prompt[0].lower() == "drop":
+                if prompt[1] in player.inventory:
+                    for item in player.inventory:
+                        if item == prompt[1]:
+                            player.dropItem(item)
+                            player.location.items.append(item)
+                elif prompt[1].lower() == "all":
+                    player.location.items.extend(player.inventory)
+                    player.dropAll()
             # returns player info
-            elif prompt.lower() == "player":
+            elif prompt[0].lower() == "player":
                 print(
                     f'\n*** Player Info ***\n\n{player}\n\n\n*** Player Info End ***\n')
-            # handles north navigation
-            elif prompt.lower() == "n":
-                print(f'\n{Colors.HEADER}Navigating......\n{Colors.ENDC}')
-                if not player.location.n_to:
-                    print('You see nothing interesting northward..')
-                else:
-                    player.setLocation(player.location.n_to)
-                    player_approaches(player)
-            # handles east navigation
-            elif prompt.lower() == "e":
-                print(f'{Colors.HEADER}Navigating...... \n{Colors.ENDC}')
-                if not player.location.e_to:
-                    print('You see nothing interesting eastward..')
-                else:
-                    player.setLocation(player.location.e_to)
-                    player_approaches(player)
-            # handles south navigation
-            elif prompt.lower() == "s":
-                print(f'{Colors.HEADER}Navigating...... \n{Colors.ENDC}')
-                if not player.location.s_to:
-                    print('You see nothing interesting eastward..')
-                else:
-                    player.setLocation(player.location.s_to)
-                    player_approaches(player)
-            # handles west navigation
-            elif prompt.lower() == "w":
-                print(f'{Colors.HEADER}Navigating...... \n{Colors.ENDC}')
-                if not player.location.w_to:
-                    print('You see nothing interesting eastward..')
-                else:
-                    player.setLocation(player.location.w_to)
-                    player_approaches(player)
+             # exit prompt
+            elif prompt[0].lower() == "help":
+                printCommands()
+            elif prompt[0].lower() == "q":
+                print("Goodbye! Thanks for playing!")
+                break
             else:
-                print(
-                    f'\n{Colors.FAIL}Command not recognized. Please try again{Colors.ENDC}\n')
+                printError()
 
         except ValueError:
             print('error')
--- src/player.py
+++ src/player.py
@@ -3,7 +3,7 @@
 
 
 class Player:
-    def __init__(self, name, location, health=100, score=0, inventory=['pickaxe', 'map', 'bronze sword', 'compass']):
+    def __init__(self, name, location, health=100, score=0, inventory=['pickaxe', 'map', 'bronze_sword', 'compass']):
         self.name = name
         self.location = location
         self.health = health
@@ -14,5 +14,28 @@ class Player:
     def setLocation(self, location):
         self.location = location
 
+    # drop item
+    def dropItem(self, item):
+        self.inventory.remove(item)
+        print(f'\nYou place the {item} on the ground')
+        print(f'{self.inventory}\n')
+
+    # drop all items
+    def dropAll(self):
+        self.inventory = []
+        print(f'\nYou place all your items on the ground')
+
+    # pickup item
+    def pickUp(self, item):
+        self.inventory.append(item)
+        print(f'\nYou place the {item} in your inventory')
+        print(f'{self.inventory}\n')
+
+    # pickup all items
+    def pickUpAll(self, items):
+        self.inventory = self.inventory.extend(items)
+        print(f'\nYou place the {items} in your inventory')
+        print(f'{self.inventory}\n')
+
     def __str__(self):
-        return f'Name: {self.name} \nLocation: {self.location} \nHealth: {self.health} \nScore: {self.score} \nInventory: {self.inventory}'
+        return f'Name: {self.name} \nLocation: {self.location.name} \nHealth: {self.health} \nScore: {self.score} \nInventory: {self.inventory}'
